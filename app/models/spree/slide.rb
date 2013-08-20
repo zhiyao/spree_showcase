e5ad5eec7d1465f5146f98290187f070c071c150
Kenny Chan Zhi Yao <zhiyao.chan@gmail.com>
2013-08-21 00:11:05 +0800

    update spree slide model to use the spree functions available in 2-0-stable

--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -2,25 +2,17 @@ module Spree
   class Slide < ActiveRecord::Base
 
     has_attached_file :image,
-      :styles=>{:thumb=> ["#{SpreeShowcase::Config.thumbnail_style}"],
-                :showcase=> ["#{SpreeShowcase::Config.showcase_style}"]},
-      :url => '/spree/showcase/:id/:style/:basename.:extension',
-      :path => ':rails_root/public/spree/showcase/:id/:style/:basename.:extension',
-      :convert_options => { :all => '-strip' }
+                      :styles=>{:thumb=> ["#{SpreeShowcase::Config.thumbnail_style}"],
+                                :showcase=> ["#{SpreeShowcase::Config.showcase_style}"]},
+                      default_style: :showcase,
+                      :url => '/spree/showcase/:id/:style/:basename.:extension',
+                      :path => ':rails_root/public/spree/showcase/:id/:style/:basename.:extension',
+                      :convert_options => { :all => '-strip -auto-orient' }
 
+    include Spree::Core::S3Support
+    supports_s3 :image
 
-    s3_creds =
-      {
-        :access_key_id => Spree::Config[:s3_access_key],
-        :secret_access_key => Spree::Config[:s3_secret],
-        :bucket => Spree::Config[:s3_bucket]
-      }
-    attachment_definitions[:image][:storage] = :s3
-    attachment_definitions[:image][:s3_credentials] = s3_creds
-    attachment_definitions[:image][:bucket] = Spree::Config[:s3_bucket]
-    attachment_definitions[:image][:s3_protocol] = Spree::Config[:s3_protocol] unless Spree::Config[:s3_protocol].blank?
-    attachment_definitions[:image][:s3_host_alias] = Spree::Config[:s3_host_alias] unless Spree::Config[:s3_host_alias].blank?
-
+    attachment_definitions[:image][:url] = Spree::Config[:attachment_url]
 
     default_scope order(:position) # Slides should always be ordered by position specified by user.
     scope :published, where(:published=>true)
