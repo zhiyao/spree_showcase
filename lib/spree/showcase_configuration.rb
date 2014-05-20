3a3615c867ce68d04e4e377b9058a3d9de031ea9
Kenny Chan Zhi Yao <zhiyao.chan@gmail.com>
2014-05-20 17:28:16 +0800

    update showcase configurations

--- lib/spree/showcase_configuration.rb
+++ lib/spree/showcase_configuration.rb
@@ -1,10 +1,9 @@
-
 class Spree::ShowcaseConfiguration < Spree::Preferences::Configuration
   preference :enabled, :boolean, :default => true
   preference :thumbnail_style, :string, :default=>"220x110#"
-  preference :showcase_style, :string, :default =>"940x470#"
+  preference :showcase_style, :string, :default =>"920x320>"
   preference :number_of_slides_to_show, :integer, :default=>4
-  
+
   preference :slideshow, :boolean, :default => true
   preference :animation_type, :string, :default=>"fade"
   preference :slideshow_speed, :integer, :default=>3000
@@ -12,6 +11,5 @@ class Spree::ShowcaseConfiguration < Spree::Preferences::Configuration
   preference :randomize, :boolean, :default => false
   preference :animation_loop, :boolean, :default => true
   preference :pause_on_hover, :boolean, :default => true
-  
 end
 
