5e15b06a4880ff84a212bbf4a840603a3eef4e95
Kenny Chan Zhi Yao <zhiyao.chan@gmail.com>
2014-09-28 11:02:53 +0800

    update admin slides index page to stable-2-0 style and added published and thumbnail for a better UX

--- app/controllers/spree/admin/slides_controller.rb
+++ app/controllers/spree/admin/slides_controller.rb
@@ -2,8 +2,8 @@ module Spree
   module Admin
     class SlidesController < ResourceController
       respond_to :html
-      
-      # Copied over from the Spree Core. 
+
+      # Copied over from the Spree Core.
       def update_positions
         params[:positions].each do |id, index|
           Slide.where(:id => id).update_all(:position => index)
@@ -12,14 +12,12 @@ module Spree
         respond_to do |format|
           format.js  { render :text => 'Ok' }
         end
-      end 
+      end
 
       def index
         @slides = Slide.all # There is a default_scope order(:position) in effect.
       end
-      
-     
-  
+
     end
   end
 end
